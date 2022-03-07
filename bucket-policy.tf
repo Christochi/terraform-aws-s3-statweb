dd27d5ec149853180fc694f6ae6e28a989af0ffe
Christochi <tochukwui2@gmail.com>
2022-03-07 17:51:19 -0500

    added comments

--- bucket-policy.tf
+++ bucket-policy.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket_policy" "website_hosting" {
 
-  bucket = aws_s3_bucket.web.id
+  bucket = aws_s3_bucket.web.id # bucket's name
+
+  # policy construct
   policy = data.aws_iam_policy_document.bucket_policy.json
 
 }
@@ -10,18 +12,16 @@ data "aws_iam_policy_document" "bucket_policy" {
 
   statement {
 
-    sid = "AddPerm"
+    sid = "AddPerm" # identifier for the policy statement
 
     principals {
       type        = "*"
       identifiers = ["*"]
     }
 
-    actions = [ "s3:GetObject" ]
+    actions = [ "s3:GetObject" ] # action to allow
 
-    resources = [
-      aws_s3_bucket.web.arn,
-      "${aws_s3_bucket.web.arn}/*",
-    ]
+    # ARN of the bucket will be of format arn:aws:s3:::bucketname
+    resources = [ "${aws_s3_bucket.web.arn}/*" ]
   }
 }
