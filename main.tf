a2a395a8a798e0d1dda26cd80fdf7d03afb7faa1
Christochi <tochukwui2@gmail.com>
2022-03-07 16:45:38 -0500

    commented some file content

--- main.tf
+++ main.tf
@@ -30,32 +30,32 @@ resource "aws_s3_object" "web_files"  {
   
 }
 
-resource "aws_s3_bucket_policy" "website_hosting" {
+# resource "aws_s3_bucket_policy" "website_hosting" {
 
-  bucket = aws_s3_bucket.web.id
-  policy = data.aws_iam_policy_document.bucket_policy.json
+#   bucket = aws_s3_bucket.web.id
+#   policy = data.aws_iam_policy_document.bucket_policy.json
 
-}
+# }
 
-# generates an IAM policy document in JSON format for use with bucket
-data "aws_iam_policy_document" "bucket_policy" {
+# # generates an IAM policy document in JSON format for use with bucket
+# data "aws_iam_policy_document" "bucket_policy" {
 
-  statement {
+#   statement {
 
-    sid = "AddPerm"
+#     sid = "AddPerm"
 
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
+#     principals {
+#       type        = "*"
+#       identifiers = ["*"]
+#     }
 
-    actions = [ "s3:GetObject" ]
+#     actions = [ "s3:GetObject" ]
 
-    resources = [
-      aws_s3_bucket.web.arn,
-      "${aws_s3_bucket.web.arn}/*",
-    ]
-  }
-}
+#     resources = [
+#       aws_s3_bucket.web.arn,
+#       "${aws_s3_bucket.web.arn}/*",
+#     ]
+#   }
+# }
 
 
