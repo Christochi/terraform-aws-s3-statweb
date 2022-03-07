b99659d8955e2af39e0691f9222cab1954cd0541
Christochi <tochukwui2@gmail.com>
2022-03-07 15:05:35 -0500

    testing

--- bucket-policy.tf
+++ bucket-policy.tf
@@ -1,27 +1,27 @@
-resource "aws_s3_bucket_policy" "website_hosting" {
+# resource "aws_s3_bucket_policy" "website_hosting" {
 
-  bucket = aws_s3_bucket.web.id
-  policy = data.aws_iam_policy_document.bucket_policy.json
+#   bucket = aws_s3_bucket.web.id
+#   policy = data.aws_iam_policy_document.bucket_policy.json
 
-}
+# }
 
-# generates an IAM policy document in JSON format for use with bucket
-data "aws_iam_policy_document" "bucket_policy" {
+# # generates an IAM policy document in JSON format for use with bucket
+# data "aws_iam_policy_document" "bucket_policy" {
 
-  statement {
+#   statement {
 
-    sid = "AddPerm"
+#     sid = "AddPerm"
 
-    principals {
-      type        = "*"
-    #   identifiers = ["123456789012"]
-    }
+#     principals {
+#       type        = "*"
+#     #   identifiers = ["123456789012"]
+#     }
 
-    actions = [ "s3:GetObject" ]
+#     actions = [ "s3:GetObject" ]
 
-    resources = [
-      aws_s3_bucket.web.arn,
-      "${aws_s3_bucket.web.arn}/*",
-    ]
-  }
-}
+#     resources = [
+#       aws_s3_bucket.web.arn,
+#       "${aws_s3_bucket.web.arn}/*",
+#     ]
+#   }
+# }
--- main.tf
+++ main.tf
@@ -30,4 +30,32 @@ resource "aws_s3_object" "web_files"  {
   
 }
 
+resource "aws_s3_bucket_policy" "website_hosting" {
+
+  bucket = aws_s3_bucket.web.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+
+}
+
+# generates an IAM policy document in JSON format for use with bucket
+data "aws_iam_policy_document" "bucket_policy" {
+
+  statement {
+
+    sid = "AddPerm"
+
+    principals {
+      type        = "*"
+    #   identifiers = ["123456789012"]
+    }
+
+    actions = [ "s3:GetObject" ]
+
+    resources = [
+      aws_s3_bucket.web.arn,
+      "${aws_s3_bucket.web.arn}/*",
+    ]
+  }
+}
+
 
