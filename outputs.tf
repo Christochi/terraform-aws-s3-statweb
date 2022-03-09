514e03b9b3c5a44e7d07e68f59ca943c77b80cb7
Christochi <tochukwui2@gmail.com>
2022-03-09 01:07:28 -0500

    added a new output

--- outputs.tf
+++ outputs.tf
@@ -3,4 +3,11 @@ output "bucket-name" {
   description = "name of bucket"
   value       = aws_s3_bucket.web.id
 
-} 
+}
+
+output "web-endpoint" {
+
+  description = "s3 url of the website"
+  value       = aws_s3_bucket_website_configuration.web_config.website_endpoint
+
+}
